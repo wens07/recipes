2b59a9f65642489861a899351d2aba3e0e7da450
wens <wens.wq@gmail.com>
2014-08-13 22:23:32 +0800

    about rotate_matrix_by_90

--- library/rotate_matrix_by_90.cpp
+++ library/rotate_matrix_by_90.cpp
@@ -8,10 +8,23 @@
 //
 
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
-int main()
+//use a temp matrix to hold the result --- time: O(n^2), memeory: O(n^2)
+vector<vector<int>> rotate_matrix_by_90(vector<vector<int>> const &v)
 {
-    return 0;
+    auto n = v.size();
+    vector<vector<int>> result;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i][j] = v[n - 1 - j][i];
+        }
+    }
+    
+    return result;
 }
