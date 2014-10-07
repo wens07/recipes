bada3783606831d4795d0a688f1e7b20abb7bd42
wens <wens.wq@gmail.com>
2014-10-07 16:30:49 +0800

    add socket related lib

---
+++ library/socket.cpp
@@ -0,0 +1,9 @@
+//
+//  socket.cpp
+//  recipes_working
+//
+//  Created by wens on 10/7/14.
+//  Copyright (c) 2014 wens. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ library/socket.h
@@ -0,0 +1,17 @@
+//
+//  socket.h
+//  recipes_working
+//
+//  Created by wens on 10/7/14.
+//  Copyright (c) 2014 wens. All rights reserved.
+//
+
+#ifndef recipes_working_socket_h
+#define recipes_working_socket_h
+
+#include <unistd.h>
+#include <sys/>
+
+
+
+#endif
